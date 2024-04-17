8a04497880be072e344cf5ca44a86fac6ed16f11
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2024-04-17 14:14:13 +0000

    budget type routes added

--- budget/src/app.ts
+++ budget/src/app.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
+import budgetTypeRouter from "./api/budgetTypeRouter";
 
 export default (app: Express) => {
   // Routes
-  app.use("api/budget/",)
+  app.use("api/budget-type/", budgetTypeRouter)
 };
