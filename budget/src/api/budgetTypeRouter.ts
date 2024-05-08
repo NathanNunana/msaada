0c831f15906ac11bb7c6658761ba1e930acee5f6
Nathan Kulewoshie <kulewoshienathan@gmail.com>
2024-05-08 15:19:51 +0000

    type routes

--- budget/src/api/budgetTypeRouter.ts
+++ budget/src/api/budgetTypeRouter.ts
@@ -2,15 +2,15 @@ import { Router } from "express";
 import { BudgetTypeController } from "../controllers/budgetTypeController";
 import { Channel } from "amqplib";
 
-const budgetTypeRouter = Router();
-
 export const typesRoutes = (channel: Channel) => {
+  const budgetTypeRouter = Router();
   const budgetTypeController = new BudgetTypeController(channel);
   budgetTypeRouter.get("/", budgetTypeController.getBudgetTypes);
   budgetTypeRouter.get("/:id", budgetTypeController.getBudgetType);
   budgetTypeRouter.post("/create", budgetTypeController.createBudgetType);
   budgetTypeRouter.put("/update", budgetTypeController.updateBudgetType);
   budgetTypeRouter.delete("/delete", budgetTypeController.deleteBudgetType);
+  return budgetTypeRouter;
 };
 
-export default budgetTypeRouter;
+export default typesRoutes;
